18b43ab7d159417b74a92d42cb474c5a05e3c32a
Wasim Akram <muhammad.wasim93@gmail.com>
2020-10-05 12:49:04 +0500

    product card before stars

--- frontend/src/products.js
+++ frontend/src/products.js
@@ -10,7 +10,7 @@ const products = [
     price: 89.99,
     countInStock: 10,
     rating: 4.5,
-    numReviews: 12,
+    numReviews: 11,
   },
   {
     _id: '2',
@@ -36,7 +36,7 @@ const products = [
     price: 929.99,
     countInStock: 5,
     rating: 3,
-    numReviews: 12,
+    numReviews: 10,
   },
   {
     _id: '4',
@@ -49,7 +49,7 @@ const products = [
     price: 399.99,
     countInStock: 11,
     rating: 5,
-    numReviews: 12,
+    numReviews: 9,
   },
   {
     _id: '5',
@@ -75,8 +75,8 @@ const products = [
     price: 29.99,
     countInStock: 0,
     rating: 4,
-    numReviews: 12,
+    numReviews: 14,
   },
-]
+];
 
-export default products
+export default products;
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Row, Col } from 'react-bootstrap';
 import products from '../products';
+import Product from '../components/Product';
 
 const HomeScreen = () => {
   return (
     <>
-      <h1>Latest Products</h1>
+      <h2>Latest Products</h2>
       <Row>
         {products.map((product) => (
           <Col sm={12} md={6} lg={4} xl={3}>
-            <h2>{product.name}</h2>
+            <Product product={product} />
           </Col>
         ))}
       </Row>
