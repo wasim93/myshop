958bd8fa6e4ad9b2783e3b340ef243bf9800f1b9
Wasim Akram <muhammad.wasim93@gmail.com>
2020-11-03 18:38:17 +0500

    Admin Middleware & Get Users Endpoint

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -103,4 +103,12 @@ const updateUserProfile = expressAsyncHandler(async (req, res) => {
   }
 });
 
-export { authUser, getUserProfile, registerUser, updateUserProfile };
+// @desc    Get all users
+// @route   GET /api/users
+// @access  Private/Admin
+const getUsers = expressAsyncHandler(async (req, res) => {
+  const users = await User.find({});
+  res.json(users);
+});
+
+export { authUser, getUserProfile, registerUser, updateUserProfile, getUsers };
--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -30,4 +30,14 @@ const protect = expressAsyncHandler(async (req, res, next) => {
   }
 });
 
-export { protect };
+//Admin
+const admin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    res.status(401);
+    throw new Error('Not authorized as an Admin');
+  }
+};
+
+export { protect, admin };
