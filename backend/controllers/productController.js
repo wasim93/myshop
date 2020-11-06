15bb73aa30059a5c335e6d2c6a8b3d89f6cc28ad
Wasim Akram <muhammad.wasim93@gmail.com>
2020-11-06 18:51:34 +0500

    Create & Update Product Enpoints

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -25,6 +25,7 @@ const getProductById = expressAsyncHandler(async (req, res) => {
 // @desc Delete a product
 // @route DELETE /api/products/:id
 // @access Private/Admin
+
 const deleteProduct = expressAsyncHandler(async (req, res) => {
   const product = await Product.findById(req.params.id);
   if (product) {
@@ -36,4 +37,63 @@ const deleteProduct = expressAsyncHandler(async (req, res) => {
   }
 });
 
-export { getProducts, getProductById, deleteProduct };
+// @desc Create a product
+// @route POST /api/products
+// @access Private/Admin
+const createProduct = expressAsyncHandler(async (req, res) => {
+  const product = new Product({
+    name: 'sample name',
+    price: 0,
+    user: req.user._id,
+    image: '/images/sample.jpg',
+    brand: 'sample brand',
+    category: 'sample category',
+    countInStock: 0,
+    numReviews: 0,
+    description: 'sample description',
+  });
+
+  const createdProduct = await product.save();
+  res.status(201).json(createdProduct);
+});
+
+// @desc Update a product
+// @route PUT /api/products/:id
+// @access Private/Admin
+const updateProduct = expressAsyncHandler(async (req, res) => {
+  const {
+    name,
+    price,
+    description,
+    image,
+    brand,
+    category,
+    countInStock,
+  } = req.body;
+
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    product.name = name;
+    product.price = price;
+    product.description = description;
+    product.image = image;
+    product.brand = brand;
+    product.category = category;
+    product.countInStock = countInStock;
+
+    const updatedProduct = await product.save();
+    res.json(updatedProduct);
+  } else {
+    res.status(404);
+    throw new Error('Product not found!');
+  }
+});
+
+export {
+  getProducts,
+  getProductById,
+  createProduct,
+  deleteProduct,
+  updateProduct,
+};
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -6,10 +6,16 @@ import {
   getProducts,
   getProductById,
   deleteProduct,
+  updateProduct,
+  createProduct,
 } from '../controllers/productController.js';
 
-router.route('/').get(getProducts);
+router.route('/').get(getProducts).post(protect, admin, createProduct);
 
-router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct);
+router
+  .route('/:id')
+  .get(getProductById)
+  .delete(protect, admin, deleteProduct)
+  .put(protect, admin, updateProduct);
 
 export default router;
