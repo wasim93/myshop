2e4096d584aa23d6f4014de7dda55c617a42bb0d
Wasim Akram <muhammad.wasim93@gmail.com>
2020-11-13 12:31:37 +0500

    Card Size Updated

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -4,7 +4,7 @@ import Rating from './Rating';
 import { Link } from 'react-router-dom';
 const Product = ({ product }) => {
   return (
-    <Card className=' my-3 p-3 rounded'>
+    <Card className=' my-3 p-3 rounded myCard'>
       <Link to={`/product/${product._id}`}>
         <Card.Img src={product.image} variant='top' />
       </Link>
