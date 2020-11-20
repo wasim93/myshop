9583873488131e412a5486fd0599ed1712e57994
Wasim Akram <muhammad.wasim93@gmail.com>
2020-11-20 14:03:40 +0500

    Fixed Bugs

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -66,10 +66,14 @@ export const login = (email, password) => async (dispatch) => {
 
 export const logout = () => (dispatch) => {
   localStorage.removeItem('userInfo');
+  localStorage.removeItem('cartItems');
+  localStorage.removeItem('shippingAddress');
+  localStorage.removeItem('paymentMethod');
   dispatch({ type: USER_LOGOUT });
   dispatch({ type: USER_DETAILS_RESET });
   dispatch({ type: ORDER_LIST_MY_RESET });
   dispatch({ type: USER_LIST_RESET });
+  document.location.href = '/login';
 };
 
 export const register = (name, email, password) => async (dispatch) => {
@@ -276,6 +280,7 @@ export const updateUser = (user) => async (dispatch, getState) => {
     dispatch({ type: USER_UPDATE_SUCCESS });
 
     dispatch({ type: USER_DETAILS_SUCCESS, payload: data });
+    dispatch({ type: USER_DETAILS_RESET });
   } catch (error) {
     dispatch({
       type: USER_UPDATE_FAIL,
